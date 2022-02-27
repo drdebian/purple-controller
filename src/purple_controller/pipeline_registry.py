adc5fad217ec935b017f069e30d02bbee4baaf4c
Albert Ulmer <albert@ulmer.name>
2022-02-27 06:22:38 +0100

    moving nodes to new pipeline

--- src/purple_controller/pipeline_registry.py
+++ src/purple_controller/pipeline_registry.py
@@ -4,6 +4,7 @@ from typing import Dict
 from kedro.pipeline import Pipeline
 
 from purple_controller.pipelines import data_import as di
+from purple_controller.pipelines import data_processing as dp
 
 
 def register_pipelines() -> Dict[str, Pipeline]:
@@ -12,9 +13,12 @@ def register_pipelines() -> Dict[str, Pipeline]:
     Returns:
         A mapping from a pipeline name to a ``Pipeline`` object.
     """
-    data_import_pipeline = di.create_pipeline()
+    #data_import_pipeline = di.create_pipeline()
+    data_processing_pipeline = dp.create_pipeline()
 
     return {
-        "__default__": data_import_pipeline,
-        "di": data_import_pipeline,
+        "__default__": data_processing_pipeline,
+        # "di": data_import_pipeline,
+        "dp": data_processing_pipeline,
+
     }
--- src/purple_controller/pipelines/data_processing/pipeline.py
+++ src/purple_controller/pipelines/data_processing/pipeline.py
@@ -4,7 +4,26 @@ generated using Kedro 0.17.6
 """
 
 from kedro.pipeline import Pipeline, node
+from .nodes import get_ev_data, load_lila_config
 
 
 def create_pipeline(**kwargs):
-    return Pipeline([])
+    return Pipeline([
+        node(
+            func=load_lila_config,
+            inputs=["raw_config_lila", "raw_config_cars"],
+            outputs="config_model",
+            name="load_lila_config_node",
+        ),
+        node(
+            func=get_ev_data,
+            inputs=["ev_data", "config_model"],
+            outputs="all_cars",
+            name="get_ev_data_node",
+        ),
+
+
+
+
+
+    ])
