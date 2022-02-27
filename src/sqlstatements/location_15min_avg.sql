9f24d3393d4db03c89ff0e37a5d550698ca54b60
Albert Ulmer <albert@ulmer.name>
2022-02-27 09:52:02 +0100

    added catalog entries changed SQL queries

--- src/sqlstatements/location_15min_avg.sql
+++ src/sqlstatements/location_15min_avg.sql
@@ -1,19 +1,26 @@
---create view location_15min_avg as 
-
-with cte0 as 
-(select 
- datetime(strftime('%Y-%m-%d %H:00:00', a.TimeDate), cast(cast(strftime('%M', a.TimeDate)*4/60+1 as int)*60/4 as string)||' minutes') as TimeDate15m
- ,a.*
- from location a
+with ctea as (
+    select datetime(
+            strftime('%Y-%m-%d %H:00:00', a.TimeDate),
+            cast(
+                cast(strftime('%M', a.TimeDate) * 4 / 60 + 1 as int) * 60 / 4 as string
+            ) || ' minutes'
+        ) as TimeDate15m,
+        a.*
+    from location a
+),
+cte0 as (
+    select a.TimeDate15m as [timestamp],
+        round(avg([PV-W]), 0) as [PV-W],
+        max([Kreisel Arbeitsmodus]) as [Kreisel Arbeitsmodus],
+        round(avg([Kreisel-Ladeleistung]), 0) as [Kreisel-Ladeleistung],
+        round(avg([Kreisel-Entladeleistung]), 0) as [Kreisel-Entladeleistung],
+        round(avg([Kreisel-SOC]), 0) as [Kreisel-SOC],
+        count(*) as cntMeasurements
+    from ctea a
+    group by a.TimeDate15m
 )
-select 
-a.TimeDate15m as TimeDate
-,round(avg([PV-W]),0) as [PV-W]
-,max([Kreisel Arbeitsmodus]) as [Kreisel Arbeitsmodus]
-,round(avg([Kreisel-Ladeleistung]), 0) as [Kreisel-Ladeleistung]
-,round(avg([Kreisel-Entladeleistung]), 0) as [Kreisel-Entladeleistung]
-,round(avg([Kreisel-SOC]), 0) as [Kreisel-SOC]
-from cte0 a
-group by a.TimeDate15m
-
-
+select b.*,
+    LAG(b.[Kreisel-SOC], 1, b.[Kreisel-SOC]) over (
+        order by b.timestamp
+    ) - b.[Kreisel-SOC] as chgSOC
+from cte0 b
