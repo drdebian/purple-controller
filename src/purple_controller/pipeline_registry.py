b96c99681ef1b607a4bd3401c10fe4dd32e75241
Albert Ulmer <albert@ulmer.name>
2022-03-02 11:49:43 +0100

    created new pipeline model_direct

--- src/purple_controller/pipeline_registry.py
+++ src/purple_controller/pipeline_registry.py
@@ -5,6 +5,7 @@ from kedro.pipeline import Pipeline
 
 from purple_controller.pipelines import data_import as di
 from purple_controller.pipelines import data_processing as dp
+from purple_controller.pipelines import model_direct as md
 
 
 def register_pipelines() -> Dict[str, Pipeline]:
@@ -15,10 +16,12 @@ def register_pipelines() -> Dict[str, Pipeline]:
     """
     data_import_pipeline = di.create_pipeline()
     data_processing_pipeline = dp.create_pipeline()
+    model_direct_pipeline = md.create_pipeline()
 
     return {
         "__default__": data_import_pipeline+data_processing_pipeline,
         "data_import": data_import_pipeline,
         "data_processing": data_processing_pipeline,
+        "model_direct": model_direct_pipeline,
 
     }
