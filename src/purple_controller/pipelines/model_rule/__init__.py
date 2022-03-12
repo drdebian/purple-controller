f1bb2df12c7456b9479531a1edbd6470fb176d98
Albert Ulmer <albert@ulmer.name>
2022-03-12 05:51:00 +0100

    added new pipeline model_rule

---
+++ src/purple_controller/pipelines/model_rule/__init__.py
@@ -0,0 +1,8 @@
+"""
+This is a boilerplate pipeline 'model_rule'
+generated using Kedro 0.17.6
+"""
+
+from .pipeline import create_pipeline
+
+__all__ = ["create_pipeline"]
---
+++ src/purple_controller/pipelines/model_rule/pipeline.py
@@ -0,0 +1,10 @@
+"""
+This is a boilerplate pipeline 'model_rule'
+generated using Kedro 0.17.6
+"""
+
+from kedro.pipeline import Pipeline, node
+
+
+def create_pipeline(**kwargs):
+    return Pipeline([])
