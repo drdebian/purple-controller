70a2d479ebb412d7c0a9e6358193dd5da35016dd
Albert Ulmer <albert@ulmer.name>
2022-03-12 06:07:37 +0100

    added model_rule pipeline to registry

--- src/purple_controller/pipeline_registry.py
+++ src/purple_controller/pipeline_registry.py
@@ -6,6 +6,7 @@ from kedro.pipeline import Pipeline
 from purple_controller.pipelines import data_import as di
 from purple_controller.pipelines import data_processing as dp
 from purple_controller.pipelines import model_direct as md
+from purple_controller.pipelines import model_rule as mr
 
 
 def register_pipelines() -> Dict[str, Pipeline]:
@@ -17,6 +18,7 @@ def register_pipelines() -> Dict[str, Pipeline]:
     data_import_pipeline = di.create_pipeline()
     data_processing_pipeline = dp.create_pipeline()
     model_direct_pipeline = md.create_pipeline()
+    model_rule_pipeline = mr.create_pipeline()
 
     return {
         "__default__": model_direct_pipeline
@@ -24,6 +26,9 @@ def register_pipelines() -> Dict[str, Pipeline]:
         + data_processing_pipeline,
         "data_import": data_import_pipeline,
         "data_processing": data_processing_pipeline,
-        "model_direct": model_direct_pipeline,
+        "model_direct": model_direct_pipeline + data_import_pipeline
+        + data_processing_pipeline,
+        "model_rule": model_rule_pipeline + data_import_pipeline
+        + data_processing_pipeline,
 
     }
