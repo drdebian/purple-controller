d6192898051c455d0ec9efca61ade4de5559d80d
Albert Ulmer <albert@ulmer.name>
2022-03-24 12:09:40 +0100

    added SQL script to add additional columns in car tables due to new format

---
+++ src/sqlstatements/add_columns_car_batteryState_chargingCurrent.sql
@@ -0,0 +1,25 @@
+-- SQLite
+alter TABLE car1
+add column batteryState TEXT;
+alter TABLE car2
+add column batteryState TEXT;
+alter TABLE car3
+add column batteryState TEXT;
+alter TABLE car4
+add column batteryState TEXT;
+alter TABLE car5
+add column batteryState TEXT;
+alter TABLE car6
+add column batteryState TEXT;
+alter TABLE car1
+add column chargingCurrent FLOAT;
+alter TABLE car2
+add column chargingCurrent FLOAT;
+alter TABLE car3
+add column chargingCurrent FLOAT;
+alter TABLE car4
+add column chargingCurrent FLOAT;
+alter TABLE car5
+add column chargingCurrent FLOAT;
+alter TABLE car6
+add column chargingCurrent FLOAT;
